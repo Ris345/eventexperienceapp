22522e6c1acff1718496536da6a567eb88ac0e9a
Rowan Carlsen <rowan.carlsen@gmail.com>
2023-07-05 18:49:15 -0400

    initial svelte commit

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<h1>
+	Backstage
+	<br />
+	<span style="font-size:.5em;">Event Experience Team Coordination</span>
+</h1>
+<slot />
+
+<style>
+	h1 {
+		color: white;
+		margin-top: 0.2em;
+		line-height: 0.8em;
+	}
+	:global(body) {
+		background-color: black;
+		background-image: linear-gradient(to bottom, #000000aa, #000000aa),
+			url('/images/backstage-horizontal.jpg');
+		background-repeat: no-repeat;
+		background-size: cover;
+		height: 100%;
+	}
+	:global(html) {
+		min-height: 100vh;
+	}
+</style>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let events = [];
+</script>
+
+<main>
+	<h2>Upcoming Events</h2>
+	<div id="calendar">
+		{#if events.length > 0}
+			<!--show each event as clickable tile-->
+		{:else}
+			<div id="fallback">No upcoming events found</div>
+		{/if}
+	</div>
+</main>
+
+<style>
+	h2 {
+		color: white;
+		margin: 0.25em 0;
+	}
+	#calendar {
+		border: 1px solid black;
+		background: white;
+	}
+	#fallback {
+		font-size: 1.2em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 200px;
+	}
+</style>
