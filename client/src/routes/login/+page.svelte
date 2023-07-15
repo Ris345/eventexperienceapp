4f5ecdbc96a1a10ff83718706d2358f115d644ea
Rowan Carlsen <rowan.carlsen@gmail.com>
2023-07-15 17:16:58 -0400

    login and register pages updated with material UI components

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,29 +1,34 @@
 <script>
-
-
+	import Button, { Label } from '@smui/button';
+	import Textfield from '@smui/textfield';
+	import HelperText from '@smui/textfield/helper-text';
+	let username = '',
+		password = '';
 </script>
 
-
 <main>
-    <h2>Login</h2>
+	<h2>Login</h2>
 	<form>
-		<label>Username</label>
-		<input/>
-		<label>Password</label>
-		<input/>
-        <br/>
-		<button>Login</button>
-	</form>
+		<Textfield bind:value={username} label="Username" />
 
+		<Textfield bind:value={password} label="Password" />
+
+		<Button variant="raised" type="submit"><Label>Login</Label></Button>
+	</form>
 </main>
 
 <style>
-   label{
-	color:white
-   }
-
-   h2{
-	color: white;
-   }
-
+	form {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		width: max-content;
+		gap: 1em;
+		background: #000000bb;
+		padding: 1em;
+		box-shadow: 0px 0px 20px 20px #000000bb;
+	}
+	h2 {
+		color: white;
+		margin: 0.25em 0;
+	}
 </style>
--- client/src/routes/register/+page.svelte
+++ client/src/routes/register/+page.svelte
@@ -1,39 +1,60 @@
 <script>
-
-
+	import Button, { Label } from '@smui/button';
+	import SegmentedButton, { Segment } from '@smui/segmented-button';
+	import Textfield from '@smui/textfield';
+	import HelperText from '@smui/textfield/helper-text';
+	let invalid = false,
+		firstName = '',
+		lastName = '',
+		email = '',
+		password = '',
+		selectedRole;
+	const choices = ['Admin', 'Organizer', 'Volunteer'];
 </script>
 
-
 <main>
-    <h2>Register</h2>
+	<h2>Register</h2>
 	<form method="POST">
-		<label>First Name:</label>
-		<input/>
-		<label>Last Name:</label>
-		<input/>
-		<label>Email:</label>
-		<input/>
-		<label>Password:</label>
-        <input/>
-		<label>Admin</label>
-		<input type="radio">
-		<label>Organizer</label>
-		<input type="radio">
-		<label>Volunteer</label>
-		<input type="radio">
-		<br/>
-		<button>Register</button>
-	</form>
+		<Textfield bind:value={firstName} label="First Name" />
 
+		<Textfield bind:value={lastName} label="Last Name" />
+
+		<Textfield bind:value={email} type="email" bind:invalid updateInvalid label="Email">
+			<HelperText validationMsg slot="helper">Please enter a valid email address.</HelperText>
+		</Textfield>
+		<Textfield bind:value={password} label="Password">
+			<HelperText slot="helper">Your password must be stronger.</HelperText>
+		</Textfield>
+		<SegmentedButton
+			style="grid-column: span 2;"
+			segments={choices}
+			let:segment
+			singleSelect
+			bind:selected={selectedRole}
+		>
+			<Segment type="button" {segment}>
+				<Label>{segment}</Label>
+			</Segment>
+		</SegmentedButton>
+		<Button variant="raised" type="submit">
+			<Label>Register</Label>
+		</Button>
+	</form>
 </main>
 
 <style>
-   label{
-	color:white
-   }
-
-   h2{
-	color: white;
-   }
-
+	h2 {
+		color: white;
+		margin: 0.25em 0;
+	}
+	form {
+		display: grid;
+		grid-template-columns: auto auto;
+		justify-items: baseline;
+		width: max-content;
+		gap: 1em;
+		background: #000000bb;
+		padding: 1em;
+		box-shadow: 0px 0px 20px 20px #000000bb;
+	}
 </style>
