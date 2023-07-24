365123a4c9803179379ab4ee56d88ae0059f10ed
Dorjee <rishav345@gmail.com>
2023-07-24 11:45:26 -0400

    adding some basic concepts in the task page, user can add tasks to the list

--- client/src/routes/tasks/+page.svelte
+++ client/src/routes/tasks/+page.svelte
@@ -1,41 +1,45 @@
 <script>
-   let tasks = []
-   let list_output = ''
-   let final_word_arr = []
+	let tasks = '';
+	let tasks_array = [];
 
-   const event_tasks = (e) => {
-         // pull data from user input 
-         tasks.push(e.target.value)
-   }
+	const event_tasks = (e) => {
+		// pull data from user input
+		tasks = e.target.value;
+	};
 
-   const addtasks = (e) => {
-      // this function should add tasks to the list dynamically 
-      list_output = tasks.pop()
-      final_word_arr.push(list_output)
-      console.log(final_word_arr)
-   }
+	const addtasks = (e) => {
+		// this function should add tasks to the list dynamically
+		tasks_array.push(tasks);
+     tasks_array = tasks_array
+	};
 
 </script>
 
-<!-- user can input new tasks for the specified date
-users can add, update and delete tasks from the list as well from the back-end
-when users type something the data gets stored and when the on click is fired the user adds a tasks one by one -->
 
+<!-- Steps 
+1. EXPECTED: user types something in the input and event_tasks pulls the data ACTUAL: that's happening 
+2. Expected: when user clicks the add task button, the item gets added to the list Actual; Item is not getting added to the list correctly
+3. EXPECTED: item should display with index number and item as string actual only li tags are getting displayed and each letter is getting displayed 
+4. expected: item should keep adding to the intial list acutual; list is changed everytime the button is clicked
+-->
 
 <main>
-   {#each final_word_arr as final_word_arrs}
-     <li>{final_word_arrs}</li>
-   {/each}
-   <div>
-      <div>Dummy Calendar data here</div>
-      <h2>Tasks in relevance to the Calendar</h2>
-   </div>
-   <span><input on:input={event_tasks}/>Today's Task</span>
-   <br/>
-<button on:click={addtasks}>Add Tasks</button>
+	<ul>
+		{#each tasks_array as task, index}
+			<li>{index + 1}:  {task}</li>
+		{/each}
+	</ul>
+	<div>
+		<div>Dummy Calendar data here</div>
+		<h2>Tasks in relevance to the Calendar</h2>
+	</div>
+	<span>Today's Task<br /><input on:input={event_tasks} /></span>
+	<br />
+	<button on:click={addtasks}>Add Tasks</button>
 </main>
 
 <style>
-
-
+   li{
+     list-style-type: none;
+   }
 </style>
