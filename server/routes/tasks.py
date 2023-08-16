76788209db6857a568ad1a751127b8ae3bf4d710
Peter <peterng.918@gmail.com>
2023-08-16 14:02:11 -0400

    copied over code from my original branch

---
+++ server/routes/tasks.py
@@ -0,0 +1,46 @@
+from Schema import TaskSchema
+from queries.tasks import db_get_tasks, db_get_task, db_post_tasks
+from fastapi import (
+    Depends,
+    HTTPException,
+    status,
+    Response,
+    APIRouter,
+    Request,
+    FastAPI,
+)
+from sqlalchemy.orm import Session
+from typing import List
+from database import SessionLocal
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+router = APIRouter()
+
+
+@router.get("/tasks", response_model=List[TaskSchema.TaskSchema])
+def get_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    tasks = db_get_tasks(db, skip=skip, limit=limit)
+    return tasks
+
+
+@router.get("/tasks/{task_id}", response_model=TaskSchema.TaskSchema)
+def get_task(task_id: int, db: Session = Depends(get_db)):
+    task = db_get_task(db, task_id)
+    if task is None:
+        raise HTTPException(status_code=400, detail="task not found")
+    return task
+
+@router.post("/tasks/create", response_model=TaskSchema.TaskSchema)
+def post_task(task : TaskSchema.TaskSchema, db: Session = Depends(get_db)):
+    tasks = db_post_tasks(task.task, task.quantity, db)
+    if tasks is None:
+        raise HTTPException(status_code=400, detail="task not found")
+    return task
