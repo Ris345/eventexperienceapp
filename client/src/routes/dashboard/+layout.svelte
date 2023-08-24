f5220b632ded3768b5e9c93043bd1ee5711bcc3e
Rowan Carlsen <rowan.carlsen@gmail.com>
2023-08-24 17:54:53 -0400

    set up left menu pretty well, along with search bar

--- client/src/routes/dashboard/+layout.svelte
+++ client/src/routes/dashboard/+layout.svelte
@@ -1,17 +1,24 @@
 <script>
 	import logo from '$lib/icons/logo.svg';
+	const links = [
+		{ name: 'Dashboard', href: '#dashboard', image: 'dashboard.svg' },
+		{ name: 'Calendar', href: '#calendar', image: 'calendar.svg' },
+		{ name: 'Events', href: '#events', image: 'events.svg' },
+		{ name: 'Tasks', href: '#tasks', image: 'tasks.svg' },
+		{ name: 'Messages', href: '#messages', image: 'messages.svg' },
+		{ name: 'Settings', href: '#settings', image: 'settings.svg' }
+	];
 </script>
 
 <div id="menu">
-	<h1><img alt="logo" src={logo} /> EEA</h1>
+	<h1><img alt="logo" src={logo} />EEA</h1>
 	<nav>
 		<ul>
-			<li>Dashboard</li>
-			<li>Calendar</li>
-			<li>Events</li>
-			<li>Tasks</li>
-			<li>Messages</li>
-			<li>Settings</li>
+			{#each links as { name, href, image }}
+				<li>
+					<a style="--bgImage: url(./icons/{image});" {href}>{name}</a>
+				</li>
+			{/each}
 		</ul>
 	</nav>
 </div>
@@ -30,10 +37,17 @@
 	#menu,
 	#sidebar {
 		background-color: #0000000d;
+		min-height: 100vh;
+	}
+	#sidebar {
 		padding: 20px;
 	}
+	#menu {
+		padding: 32px 0 0 32px;
+		min-width: 12em;
+	}
 	h1 {
-		font-size: 1.1rem;
+		font-size: 16px;
 		font-weight: bold;
 		margin: 0;
 		line-height: 1em;
@@ -41,16 +55,43 @@
 		align-items: center;
 		gap: 9px;
 	}
+	h1 img {
+		width: 35px;
+		height: auto;
+	}
 	#user-pic {
 		border-radius: 100%;
 		width: 150px;
 		height: 150px;
 		background-color: orange;
 	}
-	nav ul {
-		list-style-type: none;
-		padding: 0;
+	nav {
+		margin-top: 79px;
 	}
+	nav ul {
+		padding: 0;
+		list-style-type: none;
+		display: flex;
+		flex-flow: column;
+		gap: 32px;
+	}
+	nav a {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+		color: inherit;
+	}
+	nav a::before {
+		content: '';
+		margin-right: 1em;
+		width: 20px;
+		height: 20px;
+		background-repeat: no-repeat;
+		background-image: var(--bgImage);
+		background-size: contain;
+		background-position: 50% 50%;
+	}
+
 	:global(body) {
 		display: grid;
 		grid-template-columns: auto 1fr auto;
