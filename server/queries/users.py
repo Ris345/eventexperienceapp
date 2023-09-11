0bf3a293759d74b81a95f7d727d15ed766c10df3
crgcelestin <crgcelestin@gmail.com>
2023-09-10 23:54:10 -0400

    added cors middleware

--- server/queries/users.py
+++ server/queries/users.py
@@ -8,8 +8,10 @@ from database import SessionLocal
 from schemas.users import scheme
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+
 # temporary import for token model
 from pydantic import BaseModel
+
 # should move secret and algo to a .env later
 SECRET = "b6b3ae7250c171d2bd8eb60970d2d51a46dda2eecdd30a941cd2cfcd20b0033c"
 ALGORITHM = "HS256"
@@ -20,11 +22,15 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
 class TokenData(BaseModel):
     username: str | None = None
 
+
 # init bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def get_db():
     db = SessionLocal()
@@ -44,19 +50,27 @@ def decode_token(db: Session, token):
 
 
 # helper function to hash password
+# ! no longer in use !
 def fake_hash_password(password: str):
     return "fakehashed" + password
 
-# helper function to verify db password with form data password
 
+# helper function to verify db password with form data password
+# received password  = hash stored
 def verify_password(plain_password, hashed_password):
-    print("in verify pwd", plain_password, hashed_password, pwd_context.verify(plain_password, hashed_password))
+    print(
+        "in verify pwd",
+        plain_password,
+        hashed_password,
+        pwd_context.verify(plain_password, hashed_password),
+    )
     return pwd_context.verify(plain_password, hashed_password)
 
 
 # the real hashing helper function
 def get_password_hash(password):
     return pwd_context.hash(password)
+
 
 # creating access token, goes to another file?
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -69,7 +83,9 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, SECRET, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 # to authenticate? don't know why this is needed yet
+# authenticate and return user (need to know user exists and ensure received password matches hash stored)
 def authenticate_user(username: str, password: str, db: Session = Depends(get_db)):
     user = db_get_user_by_username(db, username)
     if not user:
@@ -78,6 +94,7 @@ def authenticate_user(username: str, password: str, db: Session = Depends(get_db
         print("in autenticate user", user)
         return False
     return user
+
 
 # testing security
 async def get_current(
@@ -216,4 +233,4 @@ def db_get_user_notifications(
 ):
 """
 pw = get_password_hash("aaa123")
-print(pw)
+print(f"hash:{pw}")
