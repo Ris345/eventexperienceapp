ef7d5ddcc36e32263210f7f0560329f920da31eb
crgcelestin <crgcelestin@gmail.com>
2023-09-29 17:37:27 -0400

    localhost docker

--- Dockerfile
+++ Dockerfile
@@ -10,11 +10,12 @@ COPY ./server /code/server
 WORKDIR /code/server
 
 # cmd to run uvicorn server in container, would be typed in CLI, import server from server.main
+EXPOSE 8000
+CMD ["uvicorn", "main:server", "--host", "0.0.0.0", "--port", "8000"]
 
-CMD ["uvicorn", "server.main:server", "--host", "0.0.0.0", "--port", "80"]
 # building the docker image
 # run in app path, docker build -t eeaimage .
 # start docker container
-# docker run -d --name eeacontainer -p 80:80 eeaimage
+# docker run -d --name eeacontainer -p 8000:8000 eeaimage
 # navigate to api docs
-# http://192.168.99.100/docs
+# http://localhost:8000/docs
--- server/queries/users.py
+++ server/queries/users.py
@@ -8,6 +8,7 @@ from database import SessionLocal
 from schemas.users import scheme
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from typing import Optional
 
 # temporary import for token model
 from pydantic import BaseModel
@@ -25,7 +26,7 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
-    username: str | None = None
+    username: Optional[str]
 
 
 # init bcrypt
@@ -73,7 +74,7 @@ def get_password_hash(password):
 
 
 # creating access token, goes to another file?
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
