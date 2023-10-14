ee59e13ccf0c31e222717777cb164765037506b5
crgcelestin <crgcelestin@gmail.com>
2023-10-13 21:00:44 -0400

    token.py

--- server/queries/token.py
+++ server/queries/token.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from schemas.users import UserCreate
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from queries.users import db_get_user_by_username, fake_hash_password
 
