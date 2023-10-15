550d6a1bd6ff21392875ad58e754ae30d26efe2e
crgcelestin <crgcelestin@gmail.com>
2023-10-15 01:14:21 -0400

    alembic

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -3,7 +3,9 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from models.GroupsUsers import Base
+import database
+
+Base = database.Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
