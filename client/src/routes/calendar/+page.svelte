21404520bdb1fc302947e6fc34976ad77cd04e7e
Dorjee <rishav345@gmail.com>
2023-12-12 20:41:13 -0500

    added the month grid view on the calendar page

--- client/src/routes/calendar/+page.svelte
+++ client/src/routes/calendar/+page.svelte
@@ -1,21 +1,21 @@
-
 <script>
     import Calendar from '@event-calendar/core';
     import TimeGrid from '@event-calendar/time-grid';
-	import '@event-calendar/core/index.css';
+    import DayGrid from '@event-calendar/day-grid'; 
+    import '@event-calendar/core/index.css';
 
-    let plugins = [TimeGrid];
+    let plugins = [TimeGrid, DayGrid];
     let options = {
         view: 'timeGridWeek',
         events: [
             // your list of events
-        ]
+        ],
+        headerToolbar: {
+            start: 'prev,next ', // Add navigation buttons
+            center: 'title', // Display the title (date range)
+            end: 'timeGridWeek,timeGridDay,dayGridMonth' // Add buttons for week and day views
+        }
     };
-
-	
 </script>
 
-
-<Calendar {plugins} {options} />
-
-
+<Calendar bind:plugins bind:options />
